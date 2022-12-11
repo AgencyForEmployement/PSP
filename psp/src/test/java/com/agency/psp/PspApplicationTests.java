c52a2f2c2e042c12749b435e9c2758f9ee019fbe
Nevena Atić <74498373+nevenaatic@users.noreply.github.com>
2022-12-11 18:36:54 +0100

    Add files via upload

---
+++ psp/src/test/java/com/agency/psp/PspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agency.psp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
