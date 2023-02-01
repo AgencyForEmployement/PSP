c202d31a124652bcc608440f796e9ce901a39e37
nevenaatic <74498373+nevenaatic@users.noreply.github.com>
2023-02-01 21:38:40 +0100

    feat: change transaction status

--- psp/src/main/java/com/agency/psp/controller/BankTransactionController.java
+++ psp/src/main/java/com/agency/psp/controller/BankTransactionController.java
@@ -1,5 +1,7 @@
 package com.agency.psp.controller;
+import com.agency.psp.dtos.BankTransactionStatus;
 import com.agency.psp.dtos.PSPResponseDto;
+import com.agency.psp.enums.TransactionStatus;
 import com.agency.psp.model.BankTransaction;
 import com.agency.psp.services.BankTransactionService;
 import lombok.AllArgsConstructor;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @AllArgsConstructor
 @RestController
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BankTransactionController {
 
     private final BankTransactionService bankTransactionService;
+    private RestTemplate restTemplate;
 
     //ovo se gadja iz banke kada se zavrsi transakcija unutar iste banke
     @PostMapping
@@ -32,7 +36,12 @@ public class BankTransactionController {
                                                     .merchantOrderId(bankTransaction.getMerchantOrderId())
                                                     .build()
         );
-
+        //psp bi ovde trebao da obavesti webshop o stanju trannsakcije
+      HttpStatus response =  restTemplate.postForObject("http://localhost:8082/transactions/status", BankTransactionStatus.builder()
+            .merchantId(bankTransaction.getMerchantOrderId())
+              .status(bankTransaction.getTransactionStatus())
+              .build(), HttpStatus.class);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
 }
